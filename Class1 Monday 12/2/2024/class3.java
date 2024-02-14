7d94fd4d1401d31d59b602385afbb244319b6dc6
gaurav065 <warrior.prince652002@gmail.com>
2024-02-14 19:53:21 +0530

    changes

--- Class1 Monday 12/2/2024/class3.java
+++ Class1 Monday 12/2/2024/class3.java
@@ -1,3 +1,30 @@
 public class class3 {
+    int x;
+    int y;
+    public class3(int a, int b){
+        this.x = a;
+        this.y = b; 
+    }
+    public int count(int n){
+        int len  = 0;
+        while (n!=0) {
+            n/=10;
+            len++;
+        }
+        return len;
+    }
+    public int dis(int num, int len){
+        int temp = num;
+        int sum = 0;
+        while(temp!=0){
+            int digit  = temp%10;
+            sum += sum + digit^len;
+            temp/ =10;
+            len--;
+        }
+        return sum;
+    }
+
+    
     
 }
