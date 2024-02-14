cdd4bd6256a85fa3840dced2f9dc884aed2107e3
gaurav065 <warrior.prince652002@gmail.com>
2024-02-14 19:07:28 +0530

    constructor, simple defination and question

---
+++ Class1 Monday 12/2/2024/class2.java
@@ -0,0 +1,12 @@
+public class class2 {
+
+    public static double area(double radius){
+        return radius;
+    }
+    public void print_f(){
+        System.out.println("hey this is a void type fucntion");
+    }
+    public static void main(String[] args) {
+        
+    }
+}
